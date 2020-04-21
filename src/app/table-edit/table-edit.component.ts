451b5c67cb21ba3a977c34c4a36519ae73357072
vuhoaiphuong <vuhoaiphuong912@gmail.com>
2020-04-21 12:05:24 +0700

    4 files modified

--- src/app/table-edit/table-edit.component.ts
+++ src/app/table-edit/table-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CustomerService } from '../customer.service';
+import { Customer } from '../Customer';
 
 @Component({
   selector: 'app-table-edit',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-edit.component.css']
 })
 export class TableEditComponent implements OnInit {
-
-  constructor() { }
+customer: Customer;
+  constructor(
+     private route: ActivatedRoute,
+    private customerService: CustomerService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+     this.getCustomer();
   }
-
+  getCustomer(){
+    this.route.params.subscribe(param => {
+        this.customerService.getCustomer(param.customerID).subscribe(data => this.customer = data);
+    })
+  }
+  updateCustomer(){
+      this.customerService.updateCustomer(this.customer).subscribe(data => {
+        this.router.navigateByUrl("/admin/product-manager")
+      });
+  }
 }
